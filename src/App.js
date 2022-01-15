3088674ff6475de97fbfa108fceaa8f2c3806dd0
Carter Day <carter.day96@outlook.com>
2022-01-15 10:01:22 -0500

    create pages routes and navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Explore from "./pages/Explore"
+import Offers from "./pages/Offers"
+import Profile from "./pages/Profile"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+import ForgotPassword from "./pages/ForgotPassword"
+
+import Navbar from "./components/Navbar"
+
 function App() {
 	return (
 		<div>
-			<h1>House Marketplace App</h1>
+			<Router>
+				<Routes>
+					<Route path="/" element={<Explore />} />
+					<Route path="/offers" element={<Offers />} />
+					{/* This will need to become a protected route */}
+					<Route path="/profile" element={<SignIn />} />
+					<Route path="/sign-in" element={<SignIn />} />
+					<Route path="/sign-up" element={<SignUp />} />
+					<Route
+						path="/forgot-password"
+						element={<ForgotPassword />}
+					/>
+				</Routes>
+				<Navbar />
+			</Router>
 		</div>
 	)
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,93 @@
+import React from "react"
+import { useNavigate, useLocation } from "react-router-dom"
+import { ReactComponent as OfferIcon } from "../assets/svg/localOfferIcon.svg"
+import { ReactComponent as ExploreIcon } from "../assets/svg/exploreIcon.svg"
+import { ReactComponent as PersonOutlineIcon } from "../assets/svg/personOutlineIcon.svg"
+
+function Navbar() {
+	const navigate = useNavigate()
+	const location = useLocation()
+
+	// Simple function to check if route match page currently on for conditional icon fill
+	const pathMatchRoute = (route) => {
+		if (route === location.pathname) {
+			return true
+		}
+	}
+
+	return (
+		<footer className="navbar">
+			<nav className="navbarNav">
+				<ul className="navbarListItems">
+					<li
+						classname="navbarListItem"
+						onClick={() => navigate("/")}
+					>
+						<ExploreIcon
+							fill={pathMatchRoute("/") ? "#9b111e" : "#8f8f8f"}
+							width="36px"
+							height="36px"
+						/>
+						<p
+							className={
+								pathMatchRoute("/")
+									? "navbarListItemNameActive"
+									: "navbarListName"
+							}
+						>
+							Explore
+						</p>
+					</li>
+					<li
+						classname="navbarListItem"
+						onClick={() => navigate("/offers")}
+					>
+						<OfferIcon
+							fill={
+								pathMatchRoute("/offers")
+									? "#9b111e"
+									: "#8f8f8f"
+							}
+							width="36px"
+							height="36px"
+						/>
+						<p
+							className={
+								pathMatchRoute("/offers")
+									? "navbarListItemNameActive"
+									: "navbarListName"
+							}
+						>
+							Offers
+						</p>
+					</li>
+					<li
+						classname="navbarListItem"
+						onClick={() => navigate("/profile")}
+					>
+						<PersonOutlineIcon
+							fill={
+								pathMatchRoute("/profile")
+									? "#9b111e"
+									: "#8f8f8f"
+							}
+							width="36px"
+							height="36px"
+						/>
+						<p
+							className={
+								pathMatchRoute("/profile")
+									? "navbarListItemNameActive"
+									: "navbarListName"
+							}
+						>
+							Profile
+						</p>
+					</li>
+				</ul>
+			</nav>
+		</footer>
+	)
+}
+
+export default Navbar
