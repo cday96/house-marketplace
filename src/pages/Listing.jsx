f8ec56afc43a7cce7fb4f0e9cb082abe86ceef7a
Carter Day <carter.day96@outlook.com>
2022-01-27 18:43:00 -0500

    add slider to listing page with swiper pckg

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -2,11 +2,16 @@ import React from "react"
 import { useState, useEffect } from "react"
 import { Link, useNavigate, useParams } from "react-router-dom"
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet"
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from "swiper"
+import { Swiper, SwiperSlide } from "swiper/react"
+import "swiper/swiper-bundle.css"
 import { getDoc, doc } from "firebase/firestore"
 import { getAuth } from "firebase/auth"
 import { db } from "../firebase.config"
 import Spinner from "../components/Spinner"
 import shareIcon from "../assets/svg/shareIcon.svg"
+
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y])
 
 function Listing() {
 	const [listing, setListing] = useState(null)
@@ -47,6 +52,20 @@ function Listing() {
 		<div>
 			<main>
 				{/* Slider will go here */}
+				<Swiper slidesPerView={1} pagination={{ clickable: true }}>
+					{listing.imageUrls.map((url, index) => (
+						<SwiperSlide key={index}>
+							<div
+								className="swiperSlideDiv"
+								style={{
+									background: `url(${listing.imageUrls[index]}) center no-repeat`,
+									backgroundSize: "cover",
+								}}
+							></div>
+						</SwiperSlide>
+					))}
+				</Swiper>
+
 				<div className="shareIconDiv" onClick={handleShare}>
 					<img src={shareIcon} alt="share icon" />
 				</div>
